0e2d05a807d303f9df571e6525fe698497610157
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-01-30 14:07:49 +0200

    getting hand position

--- Assets/Game/Scripts/Printer.cs
+++ Assets/Game/Scripts/Printer.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class Printer : MonoBehaviour
 {
     public void Print(string message)
     {
         print(message);
+    }
+
+    public void AttachToInteractor(SelectEnterEventArgs args)
+    {
+        print(args.interactorObject.GetAttachTransform(args.interactableObject).position);
+        
+        // Destroy(args.interactorObject.transform.gameObject);
+        
+        // args.interactableObject.transform.SetParent(
+        //     args.interactorObject.GetAttachTransform());
     }
 }
