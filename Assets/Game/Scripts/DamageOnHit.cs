4ab9d7a240ca5c364035786a7603b9e1865e634f
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-01-31 00:25:49 +0200

    .

---
+++ Assets/Game/Scripts/DamageOnHit.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class DamageOnHit : MonoBehaviour
+{
+    [SerializeField] private float damageAmount;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //check if the collider is an enemy
+        if (true)
+        {
+            //deal damage
+        }
+        
+        Destroy(gameObject);
+    }
+}
